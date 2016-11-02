7146a4a834eab23f9ea3709e7ce0d914b4125791
Klepnev <klepnev-ea@yandex.ru>
2016-11-02 18:48:59 +0300

    12

--- js/order.js
+++ js/order.js
@@ -21,10 +21,28 @@
  					data: data
 
  				}).done(function(data){
- 						console.log(data);
+ 						var jsoned = JSON.parse(data);
  						$('#success').addClass('active');
- 				})
+ 						console.log(jsoned);
+
+ 						var make_table = makeTable(jsoned.name, jsoned.phone, jsoned.street, jsoned.home, jsoned.part, jsoned.floor, jsoned.payment)
+  					$('#table__order').html(make_table);				
+ 				});
+
+
  		};
+
+ 		function makeTable(mame, phone, street, home, part, floor, payment) {
+ 			return "<tr><td> Ваше имя</td><td>" + mame + "</td></tr>\
+ 							<tr><td> телефон </td><td>" + phone + "</td></tr>\
+ 							<tr><td> Улица </td><td>" + street + "</td></tr>\
+ 							<tr><td> Дом </td><td>" + home + "</td></tr>\
+ 							<tr><td> Корпус </td><td>" + part + "</td></tr>\
+ 							<tr><td> Этаж </td><td>" + floor + "</td></tr>\
+ 							<tr><td> Способ оплаты </td><td>" + payment + "</td></tr>"
+ 		}
+
+
 
 
  	return {
--- php/action.php
+++ php/action.php
@@ -1,3 +1,48 @@
 <?php
-print_r($_REQUEST);
-//echo $_POST['order_name'];
+//print_r($_REQUEST);
+require "../phpmailer/PHPMailerAutoload.php";
+
+$user = [
+	'name' => $_REQUEST['name'],
+	'phone' => $_REQUEST['phone'],
+	'street' => $_REQUEST['street'],
+	'home' => $_REQUEST['home'],
+	'part' => $_REQUEST['part'],
+	'appt' => $_REQUEST['appt'],
+	'floor' => $_REQUEST['floor'],
+	'comment' => $_REQUEST['comment'],
+	'payment' => $_REQUEST['payment']
+];
+
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'burder.burder@yandex.ru';                 // SMTP username
+$mail->Password = 'Xtvgbjy159';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('burder.burder@yandex.ru', 'Отправитель');
+$mail->addAddress('burder.burder@yandex.ru', 'Получатель');     // Add a recipient
+$mail->addReplyTo('burder.burder@yandex.ru', 'Information');
+
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Сообщение от пользователя';
+$mail->Body    = 'Сообщение от пользователя: '. $user['name']. '<br> телефон: '. $user['phone']. '<br> Улица: ' . $user['street']. '<br> Дом: ' . $user['home'].'<br> Корпус: ' . $user['part'].'<br> Квартира: ' . $user['appt'].'<br> Этаж: ' . $user['floor'].'<br> Комментарии: ' . $user['comment']. '<br><br>Способ оплаты: ' . $user['payment'];
+$mail->AltBody = 'Альтернативный текст';
+
+echo json_encode($user);
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+	
+}
